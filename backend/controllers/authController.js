1804bf1463fe7322a4467603576cab4b49888e21
Josthna07 <josthna@gmail.com>
2026-06-19 17:35:12 +0530

    Complete user registration API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,7 +29,47 @@ const registerUser = async (req, res) => {
       email: user.email,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// Login User
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+
+  } catch (error) {
     res.status(500).json({
       message: error.message,
     });
@@ -38,4 +78,5 @@ const registerUser = async (req, res) => {
 
 module.exports = {
   registerUser,
+  loginUser,
 };

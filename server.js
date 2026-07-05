833b31cfc7c4b667501b27022bfb05bbb6c31396
Lakshmi Jahnavi Vuddagiri <vuddagirilakshmijahnavi@gmail.com>
2026-07-05 17:05:17 +0530

    Completed Workspace CRUD and Invite Member API

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 require("dotenv").config();
-
+const mongoose = require("mongoose");
 const express = require("express");
 const connectDB = require("./backend/config/db");
+const User = require("./backend/models/User");
 
-connectDB();
 
+const workspaceRoutes = require("./backend/routes/workspaceRoutes");
+const boardRoutes = require("./backend/routes/boardRoutes");
+(async () => {
+  await connectDB();
+  setTimeout(async () => {
+  const users = await mongoose.connection.db
+    .collection("users")
+    .find({})
+    .toArray();
+}, 5000);
+  console.log("Database:", mongoose.connection.name);
+
+  const collections = await mongoose.connection.db.listCollections().toArray();
+
+  const users = await mongoose.connection.db
+    .collection("users")
+    .find({})
+    .toArray();
+})();
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+app.use(express.json());
+
+app.use("/api/workspaces", workspaceRoutes);
+app.use("/api/boards", boardRoutes);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
